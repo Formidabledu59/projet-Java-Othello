c9936de890d2c0de55be404dc755177777ceef3f
Ayoub Fathallah <130578160+Formidabledu59@users.noreply.github.com>
2024-10-09 16:53:07 +0200

    class pawn basic

--- src/othello/pawn/Pawn.java
+++ src/othello/pawn/Pawn.java
@@ -1,3 +1,20 @@
 public class Pawn {
-    
+    private String color; // "black" ou "white"
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    // Change la couleur du pion
+    public void changeColor() {
+        if (this.color.equals("black")) {
+            this.color = "white";
+        } else {
+            this.color = "black";
+        }
+    }
+
+    public String getColor() {
+        return this.color;
+    }
 }
