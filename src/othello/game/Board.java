e862dc75d0b81c24d3eb92c4ef0b9e9957294614
Ayoub Fathallah <130578160+Formidabledu59@users.noreply.github.com>
2024-10-09 16:54:16 +0200

    class board basic

--- src/othello/game/Board.java
+++ src/othello/game/Board.java
@@ -1,5 +1,31 @@
-package othello.game;
-
 public class Board {
-    
+    private Pawn[][] board;
+
+    public Board() {
+        // Initialise le plateau 8x8 avec des cases vides
+        board = new Pawn[8][8];
+        initializeBoard();
+    }
+
+    // Initialisation du plateau avec les 4 pions au centre
+    private void initializeBoard() {
+        board[3][3] = new Pawn("white");
+        board[3][4] = new Pawn("black");
+        board[4][3] = new Pawn("black");
+        board[4][4] = new Pawn("white");
+    }
+
+    // Affiche le plateau
+    public void displayBoard() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == null) {
+                    System.out.print("- ");
+                } else {
+                    System.out.print(board[i][j].getColor().charAt(0) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
